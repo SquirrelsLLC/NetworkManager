0f42eafd703438e7d24727d6379b758292deccc7
Brian Meek <bmeek@airsquirrels.com>
2025-01-29 09:15:49 -0500

    Initial Commit! Begin adapting the Wifi-P2P device to inculde funtionality for devices that want to behave like a Miracast SINK.

--- src/libnm-base/nm-base.h
+++ src/libnm-base/nm-base.h
@@ -277,6 +277,13 @@ typedef enum {
                          | _NM_VLAN_FLAG_LOOSE_BINDING | _NM_VLAN_FLAG_MVRP,
 } _NMVlanFlags;
 
+typedef enum {    
+    /* Mirrors libnm's NMSettingWifiP2PWfdDevice Mode*/                                          
+    _NM_SETTING_WIFI_P2P_WFD_DEVICE_MODE_NONE        = 0,    
+    _NM_SETTING_WIFI_P2P_WFD_DEVICE_MODE_SOURCE      = 1,
+    _NM_SETTING_WIFI_P2P_WFD_DEVICE_MODE_SINK        = 2,
+} _NMSettingWifiP2pWfdDeviceMode;
+
 typedef enum {
     /* Mirrors libnm's NMSriovEswitchMode.
      * Values >= 0 mirror kernel's enum devlink_eswitch_mode. */
